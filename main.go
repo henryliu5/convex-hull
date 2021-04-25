262c12c14b5bd4a72c66219e0ae8eb71f6909fa2
Henry Liu <19509472+henryliu5@users.noreply.github.com>
2021-04-25 18:47:39 -0500

    Add file reading/output into util

--- main.go
+++ main.go
@@ -2,55 +2,75 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
 	"time"
 )
 
 func main() {
-	n := 10000000
-	start := time.Now()
-	a := make([][]float32, 0, n+4)
-	fmt.Println("allocation time", time.Since(start))
+	/**********************
+	 * Points from memory *
+	 **********************/
+	// n := 10000000
+	// start := time.Now()
+	// a := make([][]float32, 0, n+4)
+	// fmt.Println("allocation time", time.Since(start))
 
-	// actual hull
-	a = append(a, [][]float32{
-		{-1, -1},
-		{-1, 1},
-		{1, 1},
-		{1, -1},
-	}...,
-	)
+	// // actual hull
+	// a = append(a, [][]float32{
+	// 	{-1, -1},
+	// 	{-1, 1},
+	// 	{1, 1},
+	// 	{1, -1},
+	// }...,
+	// )
 
-	start = time.Now()
-	// random points inside
-	for i := 0; i < n; i++ {
-		// x := rand.Float32()*2 - 1
-		// y := rand.Float32()*2 - 1
-		// a = append(a, []float32{x, y})
+	// start = time.Now()
+	// // random points inside
+	// for i := 0; i < n; i++ {
+	// 	// x := rand.Float32()*2 - 1
+	// 	// y := rand.Float32()*2 - 1
+	// 	// a = append(a, []float32{x, y})
 
-		// Integer test due to floating point error
-		x := rand.Int() - math.MaxInt32/2
-		y := rand.Int() - math.MaxInt32/2
-		a = append(a, []float32{float32(x), float32(y)})
+	// 	// Integer test due to floating point error
+	// 	x := rand.Int() - math.MaxInt32/2
+	// 	y := rand.Int() - math.MaxInt32/2
+	// 	a = append(a, []float32{float32(x), float32(y)})
+	// }
+	// fmt.Println("creation time", time.Since(start))
+
+	/********************
+	 * Points from file *
+	 ********************/
+
+	points := parse_file("./serial_quickhull/input_points.txt")
+	a := make([][]float32, 0, 10)
+	for i := range points {
+		a = append(a, points[i][0:2])
 	}
-	fmt.Println("creation time", time.Since(start))
 
+	// Run jarvis march
 	fn_start := time.Now()
-	fmt.Println(len(seq_jarvis(a)))
+	hull1 := seq_jarvis(a)
+	fmt.Println("points on hull:", len(hull1))
 	fmt.Println("jarvis march", time.Since(fn_start))
 
+	output_points("serial_jarvis.txt", hull1)
+
+	// Run graham scan
 	a_1 := make([][]float32, len(a))
 	copy(a_1, a)
 	fn_start = time.Now()
-	hull := seq_graham_scan(a_1)
-	fmt.Println(len(hull))
+	hull2 := seq_graham_scan(a_1)
+	fmt.Println("points on hull:", len(hull2))
 	fmt.Println("graham scan", time.Since(fn_start))
 
-	// output_points("serial_graham.txt", hull)
-	// output_points("input.txt", a)
+	output_points("serial_graham.txt", hull2)
 
+	// Run chan's
 	fn_start = time.Now()
-	fmt.Println(len(seq_chans(a)))
+	hull3 := seq_chans(a)
+	fmt.Println("points on hull:", len(hull3))
 	fmt.Println("chan's", time.Since(fn_start))
+	output_points("serial_chan.txt", hull3)
+
+	output_points("input.txt", a)
 }
--- util.go
+++ util.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // Computes ab x ac
@@ -52,4 +55,46 @@ func output_points(filename string, points [][]float32) {
 	for i := 0; i < len(points); i++ {
 		fmt.Fprintf(f, "%f,%f\n", points[i][0], points[i][1])
 	}
+}
+
+// Parse file contents into memory
+func parse_file(filename string) [][2]float32 {
+	//Counts line in a file
+	count_lines := func(file_str string) int {
+		file, _ := os.Open(file_str)
+		scanner := bufio.NewScanner(file)
+		count := 0
+
+		for scanner.Scan() {
+			_ = scanner.Text()
+			count = count + 1
+		}
+		return count
+	}
+
+	file, _ := os.Open(filename)
+
+	num_lines := count_lines(filename)
+	scanner := bufio.NewScanner(file)
+
+	points := make([][2]float32, num_lines, num_lines)
+
+	i := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		lst := strings.Split(line, ",")
+
+		x_64, _ := strconv.ParseFloat(lst[0], 32)
+		y_64, _ := strconv.ParseFloat(lst[1], 32)
+
+		x := float32(x_64)
+		y := float32(y_64)
+
+		points[i][0] = x
+		points[i][1] = y
+
+		i = i + 1
+	}
+
+	return points
 }
