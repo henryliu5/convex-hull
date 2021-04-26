27144f60cb231818b7c155756e7d809662cbab05
Derayvative <raymond.hong22@gmail.com>
2021-04-25 20:34:39 -0500

    quickhull fix

--- serial_quickhull/quickhull_first_att.go
+++ serial_quickhull/quickhull_first_att.go
@@ -9,7 +9,7 @@ import (
 	"math"
 )
 
-var convex_hull map[int]bool
+var convex_hull [][2]float32
 
 //Counts line in a file
 func count_lines(file_str string) int {
@@ -24,34 +24,33 @@ func count_lines(file_str string) int {
 	return count
 }
 
-func getMaxMinPt(points [][2]float32) [2]int {
+func getMaxMinPt(points [][2]float32) [2][2]float32 {
 
 	max_pt_x := float32(-math.MaxFloat32)
 	max_pt_y := float32(-math.MaxFloat32)
-	max_pt_ind := -1
 
 	min_pt_x := float32(math.MaxFloat32)
 	min_pt_y := float32(math.MaxFloat32)
-	min_pt_ind := -1
 
 	for i := 0; i < len(points); i++{
 		pt := points[i]
 		if (pt[0] > max_pt_x || (pt[0] == max_pt_x && pt[1] > max_pt_y)){
 			max_pt_x = pt[0];
 			max_pt_y = pt[1];
-			max_pt_ind = i
 		}
 
 		if (pt[0] < min_pt_x || (pt[0] == min_pt_x && pt[1] < min_pt_y)){
 			min_pt_x = pt[0];
 			min_pt_y = pt[1];
-			min_pt_ind = i
 		}
 	}
 
-	var res [2]int
-	res[0] = max_pt_ind
-	res[1] = min_pt_ind
+	var res [2][2]float32
+	res[0][0] = max_pt_x
+	res[0][1] = max_pt_y
+
+	res[1][0] = min_pt_x
+	res[1][1] = min_pt_y
 
 	return res
 }
@@ -76,46 +75,63 @@ func point_line_dist(l1 [2]float32, l2 [2]float32, pt [2]float32) float32{
     return float32(num/den)
 }
 
-func getSide(l1 [2]float32, l2 [2]float32, p [2]float32) int {
+func side_of_line_point_is(l1 [2]float32, l2 [2]float32, p [2]float32) float32{
 	AB := []float32{l2[0]-l1[0], l2[1]-l1[1]}
 	AX := []float32{p[0]-l1[0], p[1]-l1[1]}
 	cross := AB[0] * AX[1] - AB[1] * AX[0]
-	if (cross > 0){
-		return 1
-	} else if (cross < 0){
-		return -1
-	}
-	return 0
+	return cross
 }
 
+func isInsideTriangle(A [2]float32, B [2]float32, C [2]float32, pt [2]float32) bool{
+	if ((side_of_line_point_is(A,B,pt) < 0) && (side_of_line_point_is(B,C,pt) < 0) && (side_of_line_point_is(C,A,pt) < 0)){
+		return true
+	}
+	return false
+}
 
-func hull(points[][2]float32, min_pt_index int, max_pt_index int, side int){
-	max_dist := float32(0.0)
-	ind := -1
-
-	max_pt := points[max_pt_index]
-	min_pt := points[min_pt_index]
+func hull(points[][2]float32, min_pt [2]float32, max_pt [2]float32){
+	fmt.Println(points)
+	if (len(points) == 0){
+		return
+	}
+	max_dist := float32(-1.0)
+	var furthest_pt [2]float32;
+	furthest_index := -1
 
 	for i := 0; i < len(points); i++{
 		pt := points[i]
 		dist := point_line_dist(min_pt, max_pt, pt)
-
-		if (getSide(min_pt, max_pt, pt) == side && dist > max_dist){
-			ind = i
+		if (dist > max_dist){
 			max_dist = dist
+			furthest_pt[0] = pt[0]
+			furthest_pt[1] = pt[1]
+			furthest_index = i
 		}
 	}
 
-	if (ind == -1){
-		//Add max, min
-		fmt.Println(max_pt)
-		fmt.Println(min_pt)
-		convex_hull[min_pt_index]=true
-		convex_hull[max_pt_index]=true
-	} else{
-		hull(points, ind, min_pt_index, -getSide(points[ind], min_pt, max_pt))
-		hull(points, ind, max_pt_index, -getSide(points[ind], max_pt, min_pt))
+	points = append(points[:furthest_index], points[furthest_index + 1:]...)
+
+	convex_hull = append(convex_hull, furthest_pt)
+
+	L1 := make([][2]float32, 0, 0)
+	L2 := make([][2]float32, 0, 0)
+
+	for i := 0; i < len(points); i++{
+		var pt [2]float32
+		pt[0] = points[i][0]
+		pt[1] = points[i][1]
+
+		if (isInsideTriangle(min_pt, furthest_pt, max_pt, pt)){
+			//Cannot be part of hull
+		} else if (side_of_line_point_is(min_pt, furthest_pt, pt) > 0){
+			L1 = append(L1, pt)
+		} else if (side_of_line_point_is(min_pt, furthest_pt, pt) < 0){
+			L2 = append(L2, pt)
+		}
 	}
+
+	hull(L1, furthest_pt,max_pt)
+    hull(L2, min_pt, furthest_pt)
 }
 
 func quickhull(points [][2]float32){
@@ -124,17 +140,33 @@ func quickhull(points [][2]float32){
 	var min_pt [2]float32
 	var max_pt [2]float32
 
-	min_pt[0] = points[res[0]][0]
-	min_pt[1] = points[res[0]][1]
+	min_pt[0] = res[1][0]
+	min_pt[1] = res[1][1]
 
-	max_pt[0] = points[res[1]][0]
-	max_pt[1] = points[res[1]][1]
+	max_pt[0] = res[0][0]
+	max_pt[1] = res[0][1]
 
-	max_pt_index := res[0]
-	min_pt_index := res[1]
+	convex_hull = append(convex_hull, min_pt)
+	convex_hull = append(convex_hull, max_pt)
 
-	hull(points, max_pt_index, min_pt_index, 1)
-	hull(points, max_pt_index, min_pt_index, -1)
+	above_pts := make([][2]float32, 0, 0)
+	below_pts := make([][2]float32, 0, 0)
+
+	for i := 0; i < len(points); i++{
+		var pt [2]float32
+		pt[0] = points[i][0]
+		pt[1] = points[i][1]
+
+		is_abv := is_above(min_pt, max_pt, pt)
+		if is_abv > 0 {
+			above_pts = append(above_pts, pt)
+		}else if is_abv < 0 {
+			below_pts = append(below_pts, pt)
+		}
+	}
+
+	hull(above_pts, min_pt, max_pt)
+	hull(below_pts, max_pt, min_pt)
 }
 
 func main() {
@@ -149,7 +181,6 @@ func main() {
 	points := make([][2]float32, num_lines, num_lines)
 	
 	i:=0
-	convex_hull = make(map[int]bool)
 	for scanner.Scan() {
         line := scanner.Text()
 		lst := strings.Split(line, ",")
@@ -163,7 +194,6 @@ func main() {
 		points[i][0] = x
 		points[i][1] = y
 
-		convex_hull[i] = false
 		i=i+1
 	}
 
@@ -171,10 +201,8 @@ func main() {
 
 	f, _ := os.Create("serial_qh_hull.txt")
     defer f.Close()
-	
+
 	for i := 0; i < len(convex_hull); i++{
-		if (convex_hull[i]){
-			fmt.Fprintf(f, "%f,%f\n", points[i][0], points[i][1])
-		}
+		fmt.Fprintf(f, "%f,%f\n", convex_hull[i][0], convex_hull[i][1])
 	}
 }
