c6e4ef3391f61b28a2f0c94ea1862deeb78d49af
Derayvative <raymond.hong22@gmail.com>
2021-04-29 19:11:14 -0500

    basic timing

--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
+	"flag"
 )
 
 // Run convex hull using algorithm: method
-func run_hull(points [][2]float32, method func([][2]float32) [][2]float32, name string) {
-	fn_start := time.Now()
-	hull := method(points)
-	fmt.Println("points on hull:", len(hull))
-	fmt.Println(name, time.Since(fn_start))
-	// Write hull to output
-	output_points(fmt.Sprintf("%s.txt", name), hull)
+func run_hull(points [][2]float32, method func([][2]float32) [][2]float32, name string, trials int, save_time bool, result_file string) {
+	time_total := int64(0)
+	for i := 0; i < trials; i++{
+		fn_start := time.Now()
+		hull := method(points)
+		//fmt.Println("points on hull:", len(hull))
+		//fmt.Println(name, time.Since(fn_start))
+		ns_elap := time.Since(fn_start).Nanoseconds()
+		time_total += (ns_elap)
+		// Write hull to output
+		output_points(fmt.Sprintf("%s.txt", name), hull)
+	}
+	avg_time := float64(time_total) / float64(trials)
+
+	if (save_time){
+		fmt.Println("Saving at ", result_file)
+		f, _ := os.Create(result_file)
+    	defer f.Close()
+		fmt.Fprintf(f, "%f\n", avg_time)
+	}
 }
 
 func main() {
@@ -50,21 +65,28 @@ func main() {
 	/********************
 	 * Points from file *
 	 ********************/
-	points := parse_file("./serial_quickhull/input_points.txt")
+
+	result_dir_ptr := flag.String("result_dir", "", "result directory location")
+	inputPtr := flag.String("input", "./serial_quickhull/input_points.txt", "input file location")
+	num_trials_ptr := flag.Int("trials", 1, "number of trials")
+	flag.Parse()
+
+	save_time := (*result_dir_ptr != "")
+	points := parse_file(*inputPtr)
 
 	// Run jarvis march
-	run_hull(points, seq_jarvis, "serial_jarvis")
+	run_hull(points, seq_jarvis, "serial_jarvis",*num_trials_ptr, save_time, *result_dir_ptr + "/serial_jarvis.txt")
 
 	// Run graham scan
 	a := make([][2]float32, len(points))
 	copy(a, points)
-	run_hull(a, seq_graham_scan, "serial_graham")
+	run_hull(a, seq_graham_scan, "serial_graham",*num_trials_ptr, save_time, *result_dir_ptr + "/serial_graham.txt")
 
 	// Run chan's
-	run_hull(points, seq_chans, "serial_chans")
+	run_hull(points, seq_chans, "serial_chans",*num_trials_ptr, save_time, *result_dir_ptr + "/serial_chan.txt")
 
 	// Run quickhull
-	run_hull(points, quickhull, "serial_qh")
+	run_hull(points, quickhull, "serial_qh",*num_trials_ptr, save_time, *result_dir_ptr + "/serial_qh.txt")
 
 	output_points("input.txt", points)
 }
--- quickhull.go
+++ quickhull.go
@@ -1,8 +1,24 @@
 package main
-
 import (
+	"os"
+	"bufio"
 	"math"
 )
+
+var convex_hull map[[2]float32]bool
+
+//Counts line in a file
+func count_lines(file_str string) int {
+	file, _ := os.Open(file_str)
+	scanner := bufio.NewScanner(file)
+	count := 0
+	
+	for scanner.Scan() {
+        _ = scanner.Text()
+		count = count + 1
+    }
+	return count
+}
 
 func getMaxMinPt(points [][2]float32) [2]int {
 
@@ -14,17 +30,17 @@ func getMaxMinPt(points [][2]float32) [2]int {
 	min_pt_y := float32(math.MaxFloat32)
 	min_pt_ind := -1
 
-	for i := 0; i < len(points); i++ {
+	for i := 0; i < len(points); i++{
 		pt := points[i]
-		if pt[0] > max_pt_x || (pt[0] == max_pt_x && pt[1] > max_pt_y) {
-			max_pt_x = pt[0]
-			max_pt_y = pt[1]
+		if (pt[0] > max_pt_x || (pt[0] == max_pt_x && pt[1] > max_pt_y)){
+			max_pt_x = pt[0];
+			max_pt_y = pt[1];
 			max_pt_ind = i
 		}
 
-		if pt[0] < min_pt_x || (pt[0] == min_pt_x && pt[1] < min_pt_y) {
-			min_pt_x = pt[0]
-			min_pt_y = pt[1]
+		if (pt[0] < min_pt_x || (pt[0] == min_pt_x && pt[1] < min_pt_y)){
+			min_pt_x = pt[0];
+			min_pt_y = pt[1];
 			min_pt_ind = i
 		}
 	}
@@ -36,14 +52,14 @@ func getMaxMinPt(points [][2]float32) [2]int {
 	return res
 }
 
-func is_above(l1 [2]float32, l2 [2]float32, p [2]float32) float32 {
-	AB := []float32{l2[0] - l1[0], l2[1] - l1[1]}
-	AX := []float32{p[0] - l1[0], p[1] - l1[1]}
-	cross := AB[0]*AX[1] - AB[1]*AX[0]
+func is_above(l1 [2]float32, l2 [2]float32, p [2]float32) float32{
+	AB := []float32{l2[0]-l1[0], l2[1]-l1[1]}
+	AX := []float32{p[0]-l1[0], p[1]-l1[1]}
+	cross := AB[0] * AX[1] - AB[1] * AX[0]
 	return cross
 }
 
-func point_line_dist(l1 [2]float32, l2 [2]float32, pt [2]float32) float32 {
+func point_line_dist(l1 [2]float32, l2 [2]float32, pt [2]float32) float32{
 	x0 := pt[0]
 	x1 := l1[0]
 	x2 := l2[0]
@@ -51,58 +67,60 @@ func point_line_dist(l1 [2]float32, l2 [2]float32, pt [2]float32) float32 {
 	y1 := l1[1]
 	y2 := l2[1]
 
-	num := math.Abs(float64((x2-x1)*(y1-y0) - (x1-x0)*(y2-y1)))
-	den := math.Sqrt((math.Pow(float64(x2-x1), 2) + math.Pow(float64(y2-y1), 2)))
-	return float32(num / den)
+	num := math.Abs(float64((x2-x1) * (y1-y0) - (x1-x0) * (y2-y1)))
+	den := math.Sqrt((math.Pow(float64(x2-x1),2) + math.Pow(float64(y2-y1),2)))
+    return float32(num/den)
 }
 
 func getSide(l1 [2]float32, l2 [2]float32, p [2]float32) int {
-	AB := []float32{l2[0] - l1[0], l2[1] - l1[1]}
-	AX := []float32{p[0] - l1[0], p[1] - l1[1]}
-	cross := AB[0]*AX[1] - AB[1]*AX[0]
-	if cross > 0 {
+	AB := []float32{l2[0]-l1[0], l2[1]-l1[1]}
+	AX := []float32{p[0]-l1[0], p[1]-l1[1]}
+	cross := AB[0] * AX[1] - AB[1] * AX[0]
+	if (cross > 0){
 		return 1
-	} else if cross < 0 {
+	} else if (cross < 0){
 		return -1
 	}
 	return 0
 }
 
-func hull(points [][2]float32, min_pt_index int, max_pt_index int, side int, convex_hull map[int]bool) {
+
+func hull(points[][2]float32, min_pt [2]float32, max_pt [2]float32, side int){
 	max_dist := float32(0.0)
 	ind := -1
 
-	max_pt := points[max_pt_index]
-	min_pt := points[min_pt_index]
+	new_points := make([][2]float32, 0)
 
-	for i := 0; i < len(points); i++ {
+	for i := 0; i < len(points); i++{
 		pt := points[i]
 		dist := point_line_dist(min_pt, max_pt, pt)
 
-		if getSide(min_pt, max_pt, pt) == side && dist > max_dist {
+		correct_side := getSide(min_pt, max_pt, pt) == side
+		if (correct_side && dist > max_dist){
 			ind = i
 			max_dist = dist
 		}
+
+		if (correct_side){
+			new_points = append(new_points, pt)
+		}
 	}
 
-	if ind == -1 {
+	if (ind == -1){
 		//Add max, min
-		// fmt.Println(max_pt)
-		// fmt.Println(min_pt)
-		convex_hull[min_pt_index] = true
-		convex_hull[max_pt_index] = true
-	} else {
-		hull(points, ind, min_pt_index, -getSide(points[ind], min_pt, max_pt), convex_hull)
-		hull(points, ind, max_pt_index, -getSide(points[ind], max_pt, min_pt), convex_hull)
+		convex_hull[min_pt]=true
+		convex_hull[max_pt]=true
+	} else{
+		hull(new_points, points[ind], min_pt, -getSide(points[ind], min_pt, max_pt))
+		hull(new_points, points[ind], max_pt, -getSide(points[ind], max_pt, min_pt))
 	}
 }
 
-func quickhull(points [][2]float32) [][2]float32 {
+func quickhull_inner(points [][2]float32){
 	res := getMaxMinPt(points)
 
 	var min_pt [2]float32
 	var max_pt [2]float32
-	convex_hull := make(map[int]bool)
 
 	min_pt[0] = points[res[0]][0]
 	min_pt[1] = points[res[0]][1]
@@ -110,17 +128,24 @@ func quickhull(points [][2]float32) [][2]float32 {
 	max_pt[0] = points[res[1]][0]
 	max_pt[1] = points[res[1]][1]
 
-	max_pt_index := res[0]
-	min_pt_index := res[1]
+	hull(points, max_pt, min_pt, 1)
+	hull(points, max_pt, min_pt, -1)
+}
 
-	hull(points, max_pt_index, min_pt_index, 1, convex_hull)
-	hull(points, max_pt_index, min_pt_index, -1, convex_hull)
-
-	result := make([][2]float32, 0, len(points)>>1)
+func quickhull(points [][2]float32) [][2]float32 {
+	
+	convex_hull = make(map[[2]float32]bool)
 	for i := 0; i < len(points); i++ {
-		if convex_hull[i] {
-			result = append(result, points[i])
+		convex_hull[points[i]] = false
+	}
+
+	quickhull_inner(points)
+
+	hull := make([][2]float32,0)
+	for i := 0; i < len(convex_hull); i++{
+		if (convex_hull[points[i]]){
+			hull = append(hull, points[i])
 		}
 	}
-	return result
+	return hull
 }
