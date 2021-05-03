fb21de3de9df7d25abd4b4d8a254e345a2da4981
Henry Liu <19509472+henryliu5@users.noreply.github.com>
2021-05-02 21:48:20 -0500

    Fix chan tangent bsearch

--- chan.go
+++ chan.go
@@ -27,39 +27,95 @@ func find_tangent(subhull [][2]float32, p [2]float32, order float32) [2]float32 
 	return subhull[endpoint]
 }
 
-// Works for negatives
-func mod(a, b int) int {
-	return (a%b + b) % b
+// Left of line a->b
+func above(a, b, c [2]float32) bool {
+	return cross_prod(a, b, c) > 0
 }
 
-func tangent2(subhull [][2]float32, p [2]float32, order float32) [2]float32 {
-	if len(subhull) < 3 {
-		return find_tangent(subhull, p, order)
-	}
-	l := 0
-	r := len(subhull)
-	n := len(subhull)
-	l_up := cross_prod(p, subhull[l], subhull[(l+1)%n])
-	l_down := cross_prod(p, subhull[l], subhull[mod(l-1, n)])
-	for l < r {
+// Right of line a->b
+func below(a, b, c [2]float32) bool {
+	return cross_prod(a, b, c) < 0
+}
 
-		mid := l + (r-l)/2
-		mid_up := cross_prod(p, subhull[mid], subhull[(mid+1)%n])
-		mid_down := cross_prod(p, subhull[mid], subhull[(mid-1)%n])
-		left_mid := cross_prod(p, subhull[l], subhull[mid])
-		if mid_up < 0 && mid_down < 0 {
-			return subhull[mid]
+// Binary search from point to leftmost tangent on a convex hull (not described in paper)
+// Source: http://geomalgorithms.com/a15-_tangents.html
+// Key intuition - consider points on convex hull as directed edges from V[0] -> V[1]
+// 		Use the direction of these vectors relative to P as the "order" so you can bsearch,
+//		leads to some casework
+func find_tangent_bsearch(V [][2]float32, P [2]float32, order float32) [2]float32 {
+	if len(V) < 3 {
+		return find_tangent(V, P, order)
+	}
+	// V = [][2]float32{[2]float32{-1, 1}, [2]float32{1, 1}, [2]float32{1, -1}, [2]float32{-1, -1}}
+	// Six cases
+	// if A up
+	// c down -> [a,c]
+	// c up above a -> [c,b]
+	// c up below a -> [a,c]
+	// if a down
+	// c up -> [c,b]
+	// c down below a -> [c,b]
+	// c down above a -> [a,c]
+	a := 0
+	b := len(V)
+	c := 0
+	n := len(V)
+	// Need V[N] == V[0]
+	V = append(V, V[0])
+	dnC := false
+	dnA := false
+	if above(P, V[n-1], V[0]) && !below(P, V[1], V[0]) {
+		return V[0]
+	}
+
+	lastA := a + 1
+	lastB := b + 1
+	lastC := c + 1
+	for {
+		// This case happens if the current point is on the convex hull
+		if lastA == a && lastB == b && lastC == c {
+			break
 		}
-		if (left_mid > 0) && (l_up < 0 || l_down == l_up) || (left_mid < 0 && mid_down < 0) {
-			r = mid
+		lastA = a
+		lastB = b
+		lastC = c
+
+		c = (a + b) / 2
+		dnC = below(P, V[c+1], V[c])
+		if above(P, V[c-1], V[c]) && !dnC {
+			return V[c]
+		}
+		dnA = below(P, V[a+1], V[a])
+		if dnA {
+			if !dnC {
+				b = c
+			} else {
+				if below(P, V[a], V[c]) {
+					b = c
+				} else {
+					a = c
+				}
+			}
 		} else {
-			l = mid + 1
+			if dnC {
+				a = c
+			} else {
+				if above(P, V[a], V[c]) {
+					b = c
+				} else {
+					a = c
+				}
+			}
 		}
-
-		l_down = -mid_up
-		l_up = cross_prod(p, subhull[l], subhull[(l+1)%n])
 	}
-	return subhull[l]
+	// We were on the convex hull so my leftmost is in my hull
+	for i := 0; i < len(V); i++ {
+		if P[0] == V[i][0] && P[1] == V[i][1] {
+			return V[i+1]
+		}
+	}
+	fmt.Println("failed to find!")
+	return [2]float32{-1, -1}
 }
 
 // Jarvis march on subhulls
@@ -79,7 +135,8 @@ func subhull_jarvis(points [][2]float32, subhull_sizes []int, group_size int) []
 		for i := 0; i < n_subhulls; i++ {
 			start := subhull_index
 			end := subhull_index + subhull_sizes[i]
-			candidates[i] = find_tangent(points[start:end], cur_p, 1.0)
+			// NOTE: using bsearch may lead to slightly different results due to colinearity (especially on uniform)
+			candidates[i] = find_tangent_bsearch(points[start:end], cur_p, 1.0)
 			subhull_index += subhull_sizes[i]
 		}
 
